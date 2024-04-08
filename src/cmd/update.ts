974ea81d099eaa24dfe8d5aa531a9460b8cb62ba
Russell Laboe <russell-laboe@outlook.com>
2024-04-07 20:12:50 -0600

    --amend

---
+++ src/cmd/update.ts
@@ -0,0 +1,28 @@
+import { Command } from "commander";
+import modules from "../modfile.js";
+import { TEMP_DIR } from "../constants.js";
+
+const exec = (program: Command) => {
+  program
+    .command("update")
+    .description("Update a module already in the mod manifest")
+    .argument("[git-clone-url]", "clone url for repo")
+    .argument("[revision]", "revision from the repo to use")
+    .action(handler);
+};
+export default exec;
+
+const handler = async (cloneUrl: string, revision: string = "HEAD") => {
+  if (cloneUrl) {
+    const dep = modules.find(cloneUrl);
+    if (!dep) {
+      console.log("dependency not installed");
+      return;
+    }
+
+    dep.revision = revision;
+    dep.install(TEMP_DIR);
+
+    modules.toModfile().write();
+  }
+};
